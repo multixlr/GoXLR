af11514b71c4abc479e4bb52e30b1aba106b97a8
Ed <pselok@gmail.com>
2023-02-15 03:37:04 +0400

    Initial Commit

---
+++ GoXLR/GoXLRApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoXLRApp.swift
+//  GoXLR
+//
+//  Created by Eduard Shugar on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoXLRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
